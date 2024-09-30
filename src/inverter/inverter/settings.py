dc2d11aa1cb40d4867cbd86b087929c585b896ce
ridowayne <hassanrilwan12@gmail.com>
2024-09-30 22:39:04 +0100

    few update

--- src/inverter/inverter/settings.py
+++ src/inverter/inverter/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,10 +94,11 @@ AUTH_USER_MODEL = 'buyer_worthiness.CustomUser'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/inverter',
+        conn_max_age=600
+    )
 }
 
 
